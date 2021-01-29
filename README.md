2b7c28a0b7622cc82ad4d5fb65c25578bb7183c4
Sam Suresh <sam@mu.my>
2021-01-29 09:40:15 +0800

    intial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# My Courses Moodle Plugin
+
+This plugin provide a page with all enrolled courses and progress which can be added to Moodle navigation
+
+## Usage
+
+Install the plugin from your Moodle Administration page
+Add a navigation link to your Moodle to view the page:
+https://yourmoodlelms.com/report/mycourse/index.php
+
+## Description
+
+One of the great benefit of using this plugin is it show all courses a user enrolled in one page along with the course category and current completion progress. 
+
+## Settings
+There is no setting available for this plugin
+
+## Contributing
+Feel free to contribute by:
+* Submitting translations to the [Moodle AMOS translator](https://lang.moodle.org/local/amos/view.php)
+* Testing and creating issues. But remember to check if the issues is already
+reported before creating a new one. Perhaps you can contribute to an already
+existing issue?
+* Solving issues and submitting code through Pull Requests to the 'master' branch or on a separate feature branch.
---
+++ lang/en/report_mycourses.php
@@ -0,0 +1,40 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'report_mycourses'
+ *
+ * @package    report
+ * @subpackage mycourses
+ * @copyright  2021 onwards Sam Suresh {@link https://modernlms.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'My Courses';
+$string['admin'] = 'My Courses';
+$string['privacy:metadata'] = 'The My Course reports plugin does not store any personal data.';
+
+
+$string['no'] = 'No';
+$string['course'] = 'Course Name';
+$string['currentscore'] = 'Current Score';
+$string['type'] = 'Course Category';
+$string['completion'] = 'Completion';
+$string['dateend'] = 'Course Due Date';
+
+
+
+
